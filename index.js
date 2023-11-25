f634403695d8f2df57d23ce8bba79fe483e6a353
ChrisKMBP <chrisknowles1987@me.com>
2023-11-25 10:37:17 +0000

    add html in public directry

--- index.js
+++ index.js
@@ -1,12 +1,10 @@
 const express = require('express');
 const app = express();
 
-// Route handler for the home page
-app.get('/', (req, res) => {
-    res.send('Hello World!');
-});
+// Serve static files from the public directory
+app.use(express.static('public'));
 
 // Start the server
 app.listen(3000, () => {
-    console.log('Server is running on port 3000');
+    console.log('Server test is running on port 3000');
 });
